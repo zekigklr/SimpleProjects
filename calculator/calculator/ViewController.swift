50a671a3af96c6109ff9b54fdc97d05e26388e89
zekigklr <77938037+zekigklr@users.noreply.github.com>
2022-11-21 22:23:45 +0300

    Flag Shared

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Zeki Gökler on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var label: UILabel!
+    @IBAction func numbers(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
