c197a32f5d12966a8503f0f2ffafd9723fc0fa47
zekigklr <77938037+zekigklr@users.noreply.github.com>
2022-11-26 21:30:35 +0300

    Update ViewController.swift

--- AlertProject/AlertProject/ViewController.swift
+++ AlertProject/AlertProject/ViewController.swift
@@ -16,12 +16,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
     @IBAction func signUpClicked(_ sender: Any) {
+        /**
         let alert = UIAlertController(title: "Error", message: "Username not found!", preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+         */
+        
+        if usernameText.text == "" {
+            makeAlert(titleInput: "Error", messageInput: "Username not found!")
+        }else if passwordText.text == "" {
+            makeAlert(titleInput: "Error", messageInput: "Password not found!")
+        }else if passwordText2.text == "" {
+            makeAlert(titleInput: "Error", messageInput: "Password do not match!")
+        }else {
+            makeAlert(titleInput: "Success", messageInput: "User complete.")
+        }
+    }
+    
+    func makeAlert (titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
         let okButton = UIAlertAction(title: "Ok", style: .default)
         alert.addAction(okButton)
         self.present(alert, animated: true)
