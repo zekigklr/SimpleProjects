b599b0e9eab24ac3b8ed7f2794389762c519d626
zekigklr <77938037+zekigklr@users.noreply.github.com>
2022-12-15 20:05:02 +0300

    Update HomeViewController.swift

--- Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
@@ -8,14 +8,25 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    private let homeFeedTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.dataSource = self
+        homeFeedTable.delegate = self
     }
-    
-
-    
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
+    }
 }
+
+extension HomeViewController: UITableViewDelegate, uitable
